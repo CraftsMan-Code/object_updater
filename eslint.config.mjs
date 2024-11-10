e0ad906284a9846e2ddfce2ac6207f33b0e60d02
Marcel Harvan <marcelharvan@Marcels-MBP.netis.cc>
2024-11-10 01:04:54 +0100

    TypeScript and ESLint initialization

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      eqeqeq: ["error", "smart"],
+    },
+  },
+
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
